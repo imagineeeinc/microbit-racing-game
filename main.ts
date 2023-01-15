2d87cc69396935176e928211521ab66a6b990733
Imagineee <59028351+imagineeeinc@users.noreply.github.com>
2023-01-15 13:41:11 +0000

    added nitrous speed feature

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 input.onButtonPressed(Button.A, function () {
-    car.change(LedSpriteProperty.X, -1)
+    if (car.get(LedSpriteProperty.X) > 1) {
+        car.change(LedSpriteProperty.X, -1)
+    }
 })
 input.onGesture(Gesture.ScreenDown, function () {
     game.pause()
@@ -16,21 +18,41 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     car.change(LedSpriteProperty.X, 1)
 })
+input.onGesture(Gesture.Shake, function () {
+    if (nitrousBar.length == 5) {
+        speeding = true
+        refreshRate = 50
+        imunity = 5
+    }
+})
+function start_scene () {
+    basic.showNumber(3)
+    basic.pause(250)
+    basic.showNumber(2)
+    basic.pause(250)
+    basic.showNumber(1)
+    basic.pause(250)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.pause(250)
+}
+let nitrous = 0
 let emptyObstaclesX = 0
 let score = 0
 let ticks = 0
+let imunity = 0
+let speeding = false
+let refreshRate = 0
+let nitrousBar: game.LedSprite[] = []
 let car: game.LedSprite = null
-basic.showNumber(3)
-basic.pause(250)
-basic.showNumber(2)
-basic.pause(250)
-basic.showNumber(1)
-basic.pause(250)
-basic.showArrow(ArrowNames.NorthEast)
-basic.pause(250)
+start_scene()
 car = game.createSprite(2, 4)
 car.set(LedSpriteProperty.Blink, 300)
 let obstacles: game.LedSprite[] = []
+nitrousBar = []
+let defaultRefreshRate = 500
+refreshRate = defaultRefreshRate
+speeding = false
+imunity = 0
 music.startMelody(music.builtInMelody(Melodies.Chase), MelodyOptions.ForeverInBackground)
 basic.forever(function () {
     if (ticks % 3 == 0) {
@@ -40,28 +62,50 @@ basic.forever(function () {
 basic.forever(function () {
     while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.Y) == 4) {
         obstacles.removeAt(0).delete()
-        score += 0.25
     }
     if (game.isPaused() == false) {
-        for (let value of obstacles) {
-            value.change(LedSpriteProperty.Y, 1)
+        for (let object of obstacles) {
+            object.change(LedSpriteProperty.Y, 1)
         }
         if (ticks % 3 == 0) {
             emptyObstaclesX = randint(1, 4)
             for (let index = 0; index <= 4; index++) {
-                if (index != emptyObstaclesX) {
+                if (index != emptyObstaclesX && index != 0) {
                     obstacles.push(game.createSprite(index, 0))
                 }
             }
         }
-        ticks += 1
-    }
-    for (let value2 of obstacles) {
-        if (value2.get(LedSpriteProperty.Y) == car.get(LedSpriteProperty.Y) && value2.get(LedSpriteProperty.X) == car.get(LedSpriteProperty.X)) {
-            game.setScore(Math.round(score))
-            music.stopMelody(MelodyStopOptions.Background)
-            game.gameOver()
+        if (obstacles[0].get(LedSpriteProperty.Y) == 4) {
+            score += 1
+            nitrous += 1
         }
     }
-    basic.pause(500)
+    if (speeding == true) {
+        if (nitrousBar.length > 0) {
+            let list: number[] = []
+            nitrousBar.removeAt(list.length).delete()
+        } else {
+            refreshRate = defaultRefreshRate
+            speeding = false
+        }
+    } else if (speeding == false) {
+        if (imunity != 0) {
+            imunity += -1
+        }
+    }
+    ticks += 1
+    if (4 < nitrous && nitrousBar.length < 5 && (speeding == false && imunity == 0)) {
+        nitrous = 0
+        nitrousBar.push(game.createSprite(0, 4 - nitrousBar.length))
+    }
+    if (speeding == false && imunity == 0) {
+        for (let wall of obstacles) {
+            if (wall.get(LedSpriteProperty.Y) == car.get(LedSpriteProperty.Y) && wall.get(LedSpriteProperty.X) == car.get(LedSpriteProperty.X)) {
+                game.setScore(Math.round(score) - 1)
+                music.stopMelody(MelodyStopOptions.Background)
+                game.gameOver()
+            }
+        }
+    }
+    basic.pause(refreshRate)
 })
